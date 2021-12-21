6b9a6c511a2c4b72ea3c593775ec4e48327951ed
Sarang Parikh <sarangparikh22@gmail.com>
2021-12-22 02:22:45 +0530

    removed hardhat console

--- contracts/implementations/PolygonServer.sol
+++ contracts/implementations/PolygonServer.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.8.0;
 
 import '../BaseServer.sol';
 
-import 'hardhat/console.sol';
-
 interface IPolygonBridge {
   function depositFor(address user, address token, bytes calldata depositData) external;
 }
