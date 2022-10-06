1512a21b87e663b749d33f85c1aa89428d354d08
HHK <hhk.contact@protonmail.com>
2022-10-06 15:44:52 +0200

    feat: add serversKeeper.sol

---
+++ contracts/ServersKeeper.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import {BaseServer} from "./BaseServer.sol";
+
+interface IMasterChef {
+    function pendingSushi(uint256 _pid, address _user)
+        external
+        view
+        returns (uint256);
+}
+
+interface KeeperCompatibleInterface {
+    function checkUpkeep(bytes calldata checkData)
+        external
+        view
+        returns (bool upkeepNeeded, bytes memory performData);
+
+    function performUpkeep(bytes calldata performData) external;
+}
+
+contract ServersKeeper is Ownable, KeeperCompatibleInterface {
+    IMasterChef public immutable masterchef;
+
+    uint256 public minSushiAmount;
+
+    address[] public servers;
+
+    constructor(address _masterchef, uint256 _minSushiAmount) {
+        masterchef = IMasterChef(_masterchef);
+        minSushiAmount = _minSushiAmount;
+    }
+
+    function setServers(address[] calldata _servers) external onlyOwner {
+        servers = _servers;
+    }
+
+    function checkUpkeep(bytes calldata checkData)
+        external
+        view
+        returns (bool upkeepNeeded, bytes memory performData)
+    {
+        uint256 length = servers.length;
+        for (uint256 i = 0; i < length; i++) {
+            BaseServer server = BaseServer(servers[i]);
+            if (
+                masterchef.pendingSushi(server.pid(), servers[i]) >
+                minSushiAmount
+            ) {
+                return (true, abi.encode(i));
+            }
+        }
+    }
+
+    function performUpkeep(bytes calldata performData) external {
+        uint256 serverId = abi.decode(performData, (uint256));
+        BaseServer server = BaseServer(servers[serverId]);
+        server.harvestAndBridge();
+    }
+}
